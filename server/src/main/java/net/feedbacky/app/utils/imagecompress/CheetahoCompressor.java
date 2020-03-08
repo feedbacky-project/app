8067e397ac8f81d641223db3cc61ac24df1225b4
Plajer <plajer.xyz@gmail.com>
2020-03-08 14:51:56 +0100

    Make image compression via cheetaho async

--- server/src/main/java/net/feedbacky/app/utils/imagecompress/CheetahoCompressor.java
+++ server/src/main/java/net/feedbacky/app/utils/imagecompress/CheetahoCompressor.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.async.Callback;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 import org.springframework.http.HttpStatus;
@@ -49,29 +50,40 @@ public class CheetahoCompressor implements Compressor {
     node.put("key", apiKey);
     node.put("url", Constants.IMAGE_HOST + localPath);
     node.put("lossy", 0);
-    HttpResponse<JsonNode> request = Unirest.post(baseUrl)
-            .header("Content-Type", "application/json")
-            .body(mapper.writeValueAsString(node))
-            .asJson();
+    Unirest.post(baseUrl).header("Content-Type", "application/json")
+            .body(mapper.writeValueAsString(node)).asJsonAsync(new Callback<JsonNode>() {
+      @Override
+      public void completed(HttpResponse<JsonNode> res) {
+        if(res.getStatus() == HttpStatus.OK.value()) {
+          try(InputStream in = new URL(res.getBody().getObject().getJSONObject("data").getString("destURL")).openStream()) {
+            Files.copy(in, Paths.get("storage-data/" + localPath), StandardCopyOption.REPLACE_EXISTING);
+          } catch(IOException e) {
+            e.printStackTrace();
+          }
+        } else {
+          switch(res.getStatus()) {
+            case 304:
+              throw new FeedbackyRestException(HttpStatus.INTERNAL_SERVER_ERROR, "Temporary file not available.");
+            case 400:
+              throw new FeedbackyRestException(HttpStatus.INTERNAL_SERVER_ERROR, "Invalid image URL.");
+            case 401:
+              throw new FeedbackyRestException(HttpStatus.INTERNAL_SERVER_ERROR, "Invalid ImageIO API key, contact site administrator.");
+            case 403:
+              throw new FeedbackyRestException(HttpStatus.INTERNAL_SERVER_ERROR, "ImageIO quota reached, contact site administrator.");
+            case 500:
+              throw new FeedbackyRestException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to process the image.");
+          }
+        }
+      }
 
-    if(request.getStatus() == HttpStatus.OK.value()) {
-      try(InputStream in = new URL(request.getBody().getObject().getJSONObject("data").getString("destURL")).openStream()) {
-        Files.copy(in, Paths.get("storage-data/" + localPath), StandardCopyOption.REPLACE_EXISTING);
+      @Override
+      public void failed(UnirestException e) {
       }
-    } else {
-      switch(request.getStatus()) {
-        case 304:
-          throw new FeedbackyRestException(HttpStatus.INTERNAL_SERVER_ERROR, "Temporary file not available.");
-        case 400:
-          throw new FeedbackyRestException(HttpStatus.INTERNAL_SERVER_ERROR, "Invalid image URL.");
-        case 401:
-          throw new FeedbackyRestException(HttpStatus.INTERNAL_SERVER_ERROR, "Invalid ImageIO API key, contact site administrator.");
-        case 403:
-          throw new FeedbackyRestException(HttpStatus.INTERNAL_SERVER_ERROR, "ImageIO quota reached, contact site administrator.");
-        case 500:
-          throw new FeedbackyRestException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to process the image.");
+
+      @Override
+      public void cancelled() {
       }
-    }
+    });
   }
 
 }
