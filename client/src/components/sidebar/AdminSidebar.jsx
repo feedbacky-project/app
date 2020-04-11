51903b8c2302822fc0d83f932574f855618b35bc
Plajer <plajer.xyz@gmail.com>
2020-04-11 12:25:17 +0200

    Admin sidebar support nodes removal [SaaS feature]

--- client/src/components/sidebar/AdminSidebar.jsx
+++ client/src/components/sidebar/AdminSidebar.jsx
@@ -1,9 +1,8 @@
 import React, {useContext} from 'react';
 import {Col} from "react-bootstrap";
-import {FaDiscord} from "react-icons/fa";
 import {increaseBrightness, isHexDark} from "../util/Utils";
 import AppContext from "../../context/AppContext";
-import {FaAt, FaColumns, FaQuestionCircle, FaRegCommentDots, FaRegEnvelope, FaSlidersH, FaTags, FaUsersCog} from "react-icons/all";
+import {FaAt, FaColumns, FaRegEnvelope, FaSlidersH, FaTags, FaUsersCog} from "react-icons/all";
 
 const AdminSidebar = (props) => {
     const context = useContext(AppContext);
@@ -52,22 +51,6 @@ const AdminSidebar = (props) => {
             <li>
                 <a href="#!" onClick={() => props.reRouteTo("invitations")} style={invitations}>
                     <FaRegEnvelope className="fa-sm mr-1 move-top-2px" style={invitationsIcon}/> Invitations
-                </a>
-            </li>
-            <li className="my-4"/>
-            <li>
-                <a href="https://app.feedbacky.net/b/feedbacky-official">
-                    <FaRegCommentDots className="fa-sm mr-1 text-black-50 move-top-2px"/> Feedback
-                </a>
-            </li>
-            <li>
-                <a href="https://docs.feedbacky.net">
-                    <FaQuestionCircle className="fa-sm mr-1 text-black-50 move-top-2px"/> FAQ
-                </a>
-            </li>
-            <li>
-                <a href="https://discordapp.com/invite/6qCnKh5">
-                    <FaDiscord className="fa-sm mr-1 text-black-50 move-top-2px"/> Discord Support
                 </a>
             </li>
         </ul>
