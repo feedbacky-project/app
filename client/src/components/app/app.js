e26ea50c6121d57cc7a6b525ef2a939e03b9ac71
Plajer <plajer.xyz@gmail.com>
2020-06-04 16:32:00 +0200

    Client fully migrated all class components to functional components

--- client/src/components/app/app.js
+++ client/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React, {Component, lazy, Suspense} from 'react';
+import React, {lazy, Suspense, useEffect, useState} from 'react';
 import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import axios from "axios";
 import {toast} from "react-toastify";
@@ -26,113 +26,81 @@ const UnsubscribeView = lazy(() => retry(() => import("views/unsubscribe-view"))
 
 toast.configure();
 
-class App extends Component {
+const CLIENT_VERSION = "0.2.0-beta";
+const API_ROUTE = (process.env.REACT_APP_SERVER_IP_ADDRESS || "https://app.feedbacky.net") + "/api/v1";
 
-    CLIENT_VERSION = "0.2.0-beta";
-    API_ROUTE = process.env.REACT_APP_SERVER_IP_ADDRESS + "/api/v1";
-    state = {
-        session: Cookies.get("FSID"),
-        search: {
-            filter: localStorage.getItem("searchFilter"),
-            sort: localStorage.getItem("searchSort"),
-        },
-        serviceData: {loaded: false, data: [], error: false},
-        userData: {loaded: false, data: [], error: false, loggedIn: false},
-        darkMode: (localStorage.getItem("darkMode") === 'true'),
-        theme: "#343a40",
-    };
+const App = () => {
+    const [session, setSession] = useState(Cookies.get("FSID"));
+    const [searchPrefs, setSearchPrefs] = useState({filter: localStorage.getItem("searchFilter"), sort: localStorage.getItem("searchSort")});
+    const [serviceData, setServiceData] = useState({loaded: false, data: [], error: false});
+    const [userData, setUserData] = useState({loaded: false, data: [], error: false});
+    const [darkMode, setDarkMode] = useState(localStorage.getItem("darkMode") === 'true');
+    const [theme, setTheme] = useState("#343a40");
 
-    componentDidMount() {
-        if (this.state.darkMode) {
+    useEffect(() => {
+        if (darkMode) {
             document.body.classList.add("dark");
         } else if (localStorage.getItem("darkMode") == null && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            this.setState({darkMode: true});
+            setDarkMode(true);
             document.body.classList.add("dark");
             localStorage.setItem("darkMode", "true");
         }
-        if (this.state.userData.loaded && this.state.serviceData.loaded) {
+        if (userData.loaded && serviceData.loaded) {
             return;
         }
-        axios.defaults.baseURL = this.API_ROUTE;
-        axios.defaults.headers.common["Authorization"] = "Bearer " + this.state.session;
-        if (!this.state.serviceData.loaded) {
+        axios.defaults.baseURL = API_ROUTE;
+        axios.defaults.headers.common["Authorization"] = "Bearer " + session;
+        if (!serviceData.loaded) {
             axios.get("/service/about").then(res => {
-                this.setState({
-                    serviceData: {...this.state.serviceData, loaded: true, data: res.data}
-                });
-            }).catch(() => this.setState({
-                serviceData: {...this.state.serviceData, error: true}
-            }));
+                setServiceData({...serviceData, loaded: true, loading: false, data: res.data});
+            }).catch(() => setServiceData({...serviceData, loading: false, error: true}));
         }
-        if (this.state.session == null) {
-            this.setState({
-                userData: {...this.state.userData, loaded: true}
-            });
+        if (session == null) {
+            setUserData({...userData, loaded: true});
             return;
         }
-        axios.get("/users/@me").then(res => {
-            this.setState({
-                userData: {...this.state.userData, data: res.data, loaded: true, loggedIn: true}
+        if (!userData.loaded) {
+            axios.get("/users/@me").then(res => {
+                setUserData({...userData, data: res.data, loaded: true, loggedIn: true, loading: false});
+            }).catch(err => {
+                if (err.response === undefined || err.response.status === 401 || err.response.status === 403 || (err.response.status >= 500 && err.response.status <= 599)) {
+                    setUserData({...userData, loaded: true, loggedIn: false, loading: false});
+                    return;
+                }
+                setUserData({...userData, loaded: true, loggedIn: false, loading: false, error: true});
             });
-        }).catch(err => {
-            if (err.response === undefined || err.response.status === 401 || err.response.status === 403 || (err.response.status >= 500 && err.response.status <= 599)) {
-                this.setState({
-                    userData: {...this.state.userData, loaded: true, loggedIn: false}
-                });
-                return;
-            }
-            this.setState({
-                userData: {...this.state.userData, loaded: true, loggedIn: false, error: true}
-            });
-        });
-    }
-
-    onLogin = (token) => {
-        //setting loaded to false because it was already loaded before this method call
-        this.setState({
-            session: token,
-            userData: {...this.state.userData, loaded: false},
-            serviceData: {...this.state.serviceData, loaded: false}
-        });
-        //forcing reupdate the props
-        this.componentDidMount();
+        }
+    }, [serviceData, userData]);
+    const onLogin = (token) => {
+        setSession(token);
+        //force rerender
+        setUserData({...userData, loaded: false});
+        setServiceData({...serviceData, loaded: false});
     };
-
-    onLogOut = () => {
+    const onLogOut = () => {
         Cookies.remove("FSID");
-        this.setState({
-            session: null,
-            userData: {...this.state.userData, data: [], loaded: true, loggedIn: false}
-        });
-        this.componentDidMount();
+        setSession(null);
+        setUserData({...userData, data: [], loaded: true, loggedIn: false});
     };
-
-    onFilteringUpdate = (filter, boardData, history) => {
+    const onFilteringUpdate = (filter, boardData, history) => {
         localStorage.setItem("searchFilter", filter);
-        this.setState({
-            search: {...this.state.search, filter},
-        });
-        this.boardRedirect(history, boardData);
+        setSearchPrefs({...searchPrefs, filter});
+        boardRedirect(history, boardData);
     };
-
-    onSortingUpdate = (sort, boardData, history) => {
+    const onSortingUpdate = (sort, boardData, history) => {
         localStorage.setItem("searchSort", sort);
-        this.setState({
-            search: {...this.state.search, sort}
-        });
-        this.boardRedirect(history, boardData);
+        setSearchPrefs({...searchPrefs, sort});
+        boardRedirect(history, boardData);
     };
-
-    boardRedirect = (history, boardData) => {
+    const boardRedirect = (history, boardData) => {
         history.push({
             pathname: "/brdr/" + boardData.discriminator,
             state: {_boardData: boardData},
         });
     };
-
-    getTheme = () => {
-        let color = tinycolor(this.state.theme);
-        if (this.state.darkMode) {
+    const getTheme = () => {
+        let color = tinycolor(theme);
+        if (darkMode) {
             color = color.lighten(10);
             //if still not readable, increase again
             if (tinycolor.readability(color, "#282828") < 2.5) {
@@ -141,79 +109,73 @@ class App extends Component {
         }
         return color;
     };
-
-    onDarkModeToggle = () => {
-        let darkMode = (localStorage.getItem("darkMode") === 'true');
-        localStorage.setItem("darkMode", (!darkMode).toString());
-        this.setState({
-            darkMode: !darkMode
-        });
-        if (darkMode) {
+    const onDarkModeToggle = () => {
+        let darkModeEnabled = (localStorage.getItem("darkMode") === 'true');
+        localStorage.setItem("darkMode", (!darkModeEnabled).toString());
+        setDarkMode(darkModeEnabled);
+        if (darkModeEnabled) {
             document.body.classList.remove("dark");
         } else {
             document.body.classList.add("dark");
         }
     };
-
-    render() {
-        if (this.state.serviceData.error) {
-            return <BrowserRouter><ErrorView icon={<FaDizzy className="error-icon"/>} message="Service Is Temporarily Unavailable"/></BrowserRouter>
-        }
-        return <ComponentLoader loader={<Row className="justify-content-center vertical-center"><LoadingSpinner/></Row>}
-                                loaded={this.state.serviceData.loaded && this.state.userData.loaded} component={
-            <AppContext.Provider value={{
-                user: {
-                    data: this.state.userData.data,
-                    loggedIn: this.state.userData.loggedIn,
-                    session: this.state.session,
-                    searchPreferences: this.state.search,
-                    darkMode: this.state.darkMode,
-                    onLogOut: this.onLogOut,
-                },
-                serviceData: this.state.serviceData.data,
-                onFilteringUpdate: this.onFilteringUpdate,
-                onSortingUpdate: this.onSortingUpdate,
-                onDarkModeToggle: this.onDarkModeToggle,
-                getTheme: this.getTheme,
-                theme: this.state.theme,
-                onThemeChange: theme => this.setState({theme}),
-            }}>
-                <Suspense fallback={<Row className="justify-content-center vertical-center"><LoadingSpinner/></Row>}>
-                    <Switch>
-                        <Route exact path="/" component={ProfileView}/>
-                        <Route exact path="/admin/create" component={CreateBoardView}/>
-                        <Route path="/merdr/:section" render={(props) =>
-                            <Redirect to={{
-                                pathname: "/me/" + props.match.params.section,
-                                state: props.location.state,
-                            }}/>}/>
-                        <Route path="/me/:section" component={ProfileView}/>
-                        <Route path="/me/" component={ProfileView}/>
-                        <Route path="/moderator_invitation/:code" component={ModeratorInvitation}/>
-                        <Route path="/invitation/:code" component={BoardInvitation}/>
-                        <Route path={"/b/:id/roadmap"} component={RoadmapView}/>
-                        <Route path="/b/:id" component={BoardView}/>
-                        {/* sneaky way to redirect from /b/ to /b/ but with different :id parameters, because it doesn't work */}
-                        <Route path="/brdr/:id" render={props =>
-                            <Redirect to={{
-                                pathname: "/b/" + props.match.params.id,
-                                state: props.location.state,
-                            }}/>}/>
-                        <Route path="/ba/:id" component={AdminPanelView}/>
-                        <Route path="/bardr/:id/:section" render={props =>
-                            <Redirect to={{
-                                pathname: "/ba/" + props.match.params.id + "/" + props.match.params.section,
-                                state: props.location.state,
-                            }}/>}/>
-                        <Route path="/i/:id" component={IdeaView}/>
-                        <Route path="/unsubscribe/:id/:code" component={UnsubscribeView}/>
-                        <Route path="/auth/:provider" render={() => <OauthReceiver onLogin={this.onLogin}/>}/>
-                        <Route render={() => <ErrorView icon={<FaExclamationCircle className="error-icon"/>} message="Content Not Found"/>}/>
-                    </Switch>
-                </Suspense>
-            </AppContext.Provider>
-        }/>
+    if (serviceData.error) {
+        return <BrowserRouter><ErrorView icon={<FaDizzy className="error-icon"/>} message="Service Is Temporarily Unavailable"/></BrowserRouter>
     }
-}
+    return <ComponentLoader loader={<Row className="justify-content-center vertical-center"><LoadingSpinner/></Row>}
+                            loaded={serviceData.loaded && userData.loaded} component={
+        <AppContext.Provider value={{
+            user: {
+                data: userData.data,
+                loggedIn: userData.loggedIn,
+                session: session,
+                searchPreferences: searchPrefs,
+                darkMode: darkMode,
+                onLogOut: onLogOut,
+            },
+            serviceData: serviceData.data,
+            onFilteringUpdate: onFilteringUpdate,
+            onSortingUpdate: onSortingUpdate,
+            onDarkModeToggle: onDarkModeToggle,
+            getTheme: getTheme,
+            theme: theme,
+            onThemeChange: theme => setTheme(theme),
+        }}>
+            <Suspense fallback={<Row className="justify-content-center vertical-center"><LoadingSpinner/></Row>}>
+                <Switch>
+                    <Route exact path="/" component={ProfileView}/>
+                    <Route exact path="/admin/create" component={CreateBoardView}/>
+                    <Route path="/merdr/:section" render={(props) =>
+                        <Redirect to={{
+                            pathname: "/me/" + props.match.params.section,
+                            state: props.location.state,
+                        }}/>}/>
+                    <Route path="/me/:section" component={ProfileView}/>
+                    <Route path="/me/" component={ProfileView}/>
+                    <Route path="/moderator_invitation/:code" component={ModeratorInvitation}/>
+                    <Route path="/invitation/:code" component={BoardInvitation}/>
+                    <Route path={"/b/:id/roadmap"} component={RoadmapView}/>
+                    <Route path="/b/:id" component={BoardView}/>
+                    {/* sneaky way to redirect from /b/ to /b/ but with different :id parameters, because it doesn't work */}
+                    <Route path="/brdr/:id" render={props =>
+                        <Redirect to={{
+                            pathname: "/b/" + props.match.params.id,
+                            state: props.location.state,
+                        }}/>}/>
+                    <Route path="/ba/:id" component={AdminPanelView}/>
+                    <Route path="/bardr/:id/:section" render={props =>
+                        <Redirect to={{
+                            pathname: "/ba/" + props.match.params.id + "/" + props.match.params.section,
+                            state: props.location.state,
+                        }}/>}/>
+                    <Route path="/i/:id" component={IdeaView}/>
+                    <Route path="/unsubscribe/:id/:code" component={UnsubscribeView}/>
+                    <Route path="/auth/:provider" render={() => <OauthReceiver onLogin={onLogin}/>}/>
+                    <Route render={() => <ErrorView icon={<FaExclamationCircle className="error-icon"/>} message="Content Not Found"/>}/>
+                </Switch>
+            </Suspense>
+        </AppContext.Provider>
+    }/>
+};
 
 export default App;
