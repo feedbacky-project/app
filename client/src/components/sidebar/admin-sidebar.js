903986b13c7f67ad874f107c980ccd6ec31ab4ee
Plajer <plajer.xyz@gmail.com>
2020-06-09 18:30:57 +0200

    Remove private boards from sidebar

--- client/src/components/sidebar/admin-sidebar.js
+++ client/src/components/sidebar/admin-sidebar.js
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react';
 import {Col} from "react-bootstrap";
 import AppContext from "context/app-context";
-import {FaAt, FaColumns, FaRegEnvelope, FaSlidersH, FaTags, FaUsersCog} from "react-icons/all";
+import {FaAt, FaColumns, FaSlidersH, FaTags, FaUsersCog} from "react-icons/all";
 import {renderSidebarRoutes} from "components/sidebar/sidebar-commons";
 
 const AdminSidebar = (props) => {
@@ -10,8 +10,7 @@ const AdminSidebar = (props) => {
         {tags: (data) => <React.Fragment><FaTags className="mr-1 move-top-1px" style={data}/> Tags</React.Fragment>},
         {social: (data) => <React.Fragment><FaAt className="mr-1 move-top-1px" style={data}/> Social Links</React.Fragment>},
         {webhooks: (data) => <React.Fragment><FaColumns className="mr-1 move-top-1px" style={data}/> Webhooks</React.Fragment>},
-        {moderators: (data) => <React.Fragment><FaUsersCog className="mr-1 move-top-1px" style={data}/> Moderators</React.Fragment>},
-        {invitations: (data) => <React.Fragment><FaRegEnvelope className="mr-1 move-top-1px" style={data}/> Invitations</React.Fragment>}
+        {moderators: (data) => <React.Fragment><FaUsersCog className="mr-1 move-top-1px" style={data}/> Moderators</React.Fragment>}
     ];
     const context = useContext(AppContext);
     const themeColor = context.getTheme();
