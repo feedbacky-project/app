ac45a70c673f585f048abd5e0d73fdf92de29832
Plajer <plajer.xyz@gmail.com>
2020-06-09 13:59:00 +0200

    Tags which are too dark in dark mode gets lighten now

--- client/src/components/app/page-badge.js
+++ client/src/components/app/page-badge.js
@@ -2,11 +2,16 @@ import React, {useContext} from "react";
 import PropTypes from "prop-types";
 import AppContext from "context/app-context";
 import {Badge} from "react-bootstrap";
+import tinycolor from "tinycolor2";
 
 const PageBadge = ({color, text, className = ""}) => {
     const context = useContext(AppContext);
     if (context.user.darkMode) {
         color = color.lighten(10);
+        //if still not readable, increase again
+        if (tinycolor.readability(color, "#282828") < 2.5) {
+            color = color.lighten(25);
+        }
         return <Badge color="" style={{color: color, fontWeight: "bold", backgroundColor: color.clone().setAlpha(.2)}} className={className}>{text}</Badge>
     } else {
         return <Badge color="" style={{backgroundColor: color}} className={className}>{text}</Badge>
