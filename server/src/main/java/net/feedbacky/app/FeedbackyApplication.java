1739819539f1b5c2897d7e67e67deeef4303fde1
Plajer <plajer.xyz@gmail.com>
2021-01-16 11:31:43 +0100

    Exclude userdetails auto configuration from startup, not required

--- server/src/main/java/net/feedbacky/app/FeedbackyApplication.java
+++ server/src/main/java/net/feedbacky/app/FeedbackyApplication.java
@@ -2,12 +2,13 @@ package net.feedbacky.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class FeedbackyApplication {
 
   public static final String BACKEND_VERSION = "0.6.0-beta";
