5a8657d3982625dfcaf830c3d6dd7d93900fd80d
Plajer <plajer.xyz@gmail.com>
2021-02-10 17:00:44 +0100

    Don't fetch connectedAccounts when not logged in

--- client/src/routes/profile/subroutes/SettingsSubroute.js
+++ client/src/routes/profile/subroutes/SettingsSubroute.js
@@ -56,6 +56,9 @@ const SettingsSubroute = () => {
     };
 
     useEffect(() => {
+        if(!user.loggedIn) {
+            return;
+        }
         axios.get("/users/@me/connectedAccounts").then(res => {
             if (res.status !== 200) {
                 return;
