166c0934d9e4089e9f50f53ce58f7700e0a477f7
Plajer <plajer.xyz@gmail.com>
2021-03-03 18:30:20 +0100

    Make loader more visible on light mode

--- client/src/ui/button/UiLoadableButton.js
+++ client/src/ui/button/UiLoadableButton.js
@@ -1,5 +1,7 @@
+import {LIGHT_THEME_COLOR} from "AppAppearance";
+import AppContext from "context/AppContext";
 import PropTypes from "prop-types";
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 import tinycolor from "tinycolor2";
 import {UiButton} from "ui/button/UiButton";
 import {UiLoadingSpinner} from "ui/UiLoadingSpinner";
@@ -7,10 +9,16 @@ import {UiLoadingSpinner} from "ui/UiLoadingSpinner";
 const UiLoadableButton = (props) => {
     const {children, onClick, ...otherProps} = props;
     const [loading, setLoading] = useState(false);
+    const {user} = useContext(AppContext);
+    const darkModeColor = tinycolor("#f2f2f2");
+    const lightModeColor = tinycolor(LIGHT_THEME_COLOR);
     return <UiButton disabled={loading} onClick={() => {
         setLoading(true);
         onClick().finally(() => setLoading(false));
-    }} {...otherProps}>{loading ? <React.Fragment><UiLoadingSpinner size={"sm"} color={tinycolor("#f2f2f2")} className={"mr-1"}/> <span className={"align-middle"}>{children}</span></React.Fragment> : children}</UiButton>
+    }} {...otherProps}>{loading ? <React.Fragment>
+        <UiLoadingSpinner size={"sm"} color={user.darkMode ? darkModeColor : lightModeColor} className={"mr-1"}/>
+        <span className={"align-middle"}>{children}</span>
+    </React.Fragment> : children}</UiButton>
 };
 
 UiLoadableButton.propTypes = {
