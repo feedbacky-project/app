91048420fa12287bd928ceeb51b2362bc8c5af0f
Plajer <plajer.xyz@gmail.com>
2021-03-03 18:34:51 +0100

    Fix color selector (once again)

--- client/src/components/commons/ColorSelectionHelper.js
+++ client/src/components/commons/ColorSelectionHelper.js
@@ -34,7 +34,7 @@ const ColorPicker = styled(ChromePicker)`
 `;
 
 const ColorSelectionHelper = ({title, color, setColor, colorWarning}) => {
-    const warn = colorWarning === true ? tinycolor.readability(color, "#fff") < WCAG_AA_CONTRAST || tinycolor.readability(tinycolor(color).lighten(10), "#292c30") < WCAG_AA_CONTRAST : false;
+    const warn = colorWarning === true ? tinycolor.readability(color, "#fff") < WCAG_AA_CONTRAST || tinycolor.readability(tinycolor(color).clone().lighten(10), "#292c30") < WCAG_AA_CONTRAST : false;
     const textClass = warn ? "text-red" : "";
     return <React.Fragment>
         <UiFormLabel className={textClass}>{title}</UiFormLabel>
