7656dfb9877bf569700bcc44f97de76488104b5f
Plajer <plajer.xyz@gmail.com>
2021-11-14 17:46:59 +0100

    feat: Improved navbar view for mobile/tablet users

--- client/src/components/commons/PageNavbar.js
+++ client/src/components/commons/PageNavbar.js
@@ -28,7 +28,7 @@ const GoBackButton = styled(Link)`
 `;
 
 const SelectedRoute = styled(UiNavbarSelectedOption)`
-  @media(max-width: 576px) {
+  @media(max-width: 768px) {
     padding-bottom: 13px;
   }
 `;
@@ -68,31 +68,31 @@ const PageNavbar = ({selectedNode, goBackVisible = false}) => {
         {goBackVisible && <GoBackButton theme={context.getTheme().toString()} to={{pathname: "/b/" + data.discriminator, state: {_boardData: data}}} aria-label={"Go Back"}>
             <FaChevronLeft className={"ml-2"}/>
         </GoBackButton>}
-        <UiContainer className={"d-sm-flex d-block"}>
+        <UiContainer className={"d-md-flex d-block"}>
             <UiNavbarBrand theme={context.getTheme().toString()} to={{pathname: (goBackVisible ? "/b/" + data.discriminator : "/me"), state: {_boardData: data}}}>
                 <img className={"mr-2"} src={data.logo} height={30} width={30} alt={"Board Logo"}/>
                 <span className={"align-bottom"}>{data.name}</span>
             </UiNavbarBrand>
             {renderLogIn(onNotLoggedClick, context, data)}
-            <div className={"d-sm-flex d-block my-sm-0 my-2 order-sm-1 order-2"} style={{fontWeight: "500", whiteSpace: "nowrap"}}>
+            <div className={"d-md-flex d-block my-md-0 my-2 order-md-1 order-2"} style={{fontWeight: "500", whiteSpace: "nowrap"}}>
                 {selectedNode === "admin" &&
                 <SelectedRoute to={{pathname: "/ba/" + data.discriminator, state: {_boardData: data}}}
                                theme={context.getTheme()} border={selectedNode === "admin" ? context.getTheme().setAlpha(.75) : undefined} aria-label={"Admin Panel"}>
-                    <FaRegUser className={"mr-sm-2 mr-0 mx-sm-0 mx-1"}/>
+                    <FaRegUser className={"mr-md-2 mr-0 mx-md-0 mx-1"}/>
                     <span className={"d-inline-block align-middle"}>Admin Panel</span>
                 </SelectedRoute>
                 }
                 {selectedNode !== "admin" &&
                 <FeedbackComponent to={{pathname: "/b/" + data.discriminator, state: {_boardData: data}}}
                                    theme={context.getTheme()} border={selectedNode === "feedback" ? context.getTheme().setAlpha(.75) : undefined} aria-label={"Feedback"}>
-                    <FaRegComment className={"mr-sm-2 mr-0 mx-sm-0 mx-1"}/>
+                    <FaRegComment className={"mr-md-2 mr-0 mx-md-0 mx-1"}/>
                     <span className={"d-inline-block align-middle"}>Feedback</span>
                 </FeedbackComponent>
                 }
                 {(data.roadmapEnabled && selectedNode !== "admin") &&
                 <RoadmapComponent to={{pathname: "/b/" + data.discriminator + "/roadmap", state: {_boardData: data}}}
                                   theme={context.getTheme()} border={selectedNode === "roadmap" ? context.getTheme().setAlpha(.75) : undefined} aria-label={"Roadmap"}>
-                    <FaRegMap className={"mr-sm-2 mr-0 mx-sm-0 mx-1"}/>
+                    <FaRegMap className={"mr-md-2 mr-0 mx-md-0 mx-1"}/>
                     <span className={"d-inline-block align-middle"}>Roadmap</span>
                 </RoadmapComponent>
                 }
@@ -100,7 +100,7 @@ const PageNavbar = ({selectedNode, goBackVisible = false}) => {
                 <React.Fragment>
                     <ChangelogComponent to={{pathname: "/b/" + data.discriminator + "/changelog", state: {_boardData: data}}}
                                         theme={context.getTheme()} border={selectedNode === "changelog" ? context.getTheme().setAlpha(.75) : undefined} aria-label={"Changelog"}>
-                        <FaRegListAlt className={"mr-sm-2 mr-0 mx-sm-0 mx-1"}/>
+                        <FaRegListAlt className={"mr-md-2 mr-0 mx-md-0 mx-1"}/>
                         <span className={"d-inline-block align-middle"}>Changelog</span>
                         {renderChangelogNotificationBubble()}
                     </ChangelogComponent>
