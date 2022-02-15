55de0975fed579380e58abe266b1fb3efb33e212
Plajer <plajer.xyz@gmail.com>
2022-02-15 20:35:09 +0100

    chore: Bump to 1.0.0.RC.1

--- server/src/main/java/net/feedbacky/app/FeedbackyApplication.java
+++ server/src/main/java/net/feedbacky/app/FeedbackyApplication.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class FeedbackyApplication {
 
-  public static final String BACKEND_VERSION = "1.0.0.RC.0";
+  public static final String BACKEND_VERSION = "1.0.0.RC.1";
   private static boolean devMode = false;
 
   public static void main(String[] args) {
